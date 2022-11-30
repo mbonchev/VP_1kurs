7733861ec3710b2638beb7bfc533899bebcc25a6
Martin Bonchev <marty88marty88@gmail.com>
2022-11-30 22:16:14 +0200

    New solutions

--- Zadanie 3/modifyList.py
+++ Zadanie 3/modifyList.py
@@ -7,18 +7,13 @@ x = None
 while x != 0 and x != 1:
     x = int(input("Input 0 or 1: "))
 
-outputList = []
 if x == 0:
     for i in range(n):
         if i % 2 == 0:
-            outputList.append(inputList[i] + 5)
-        else:
-            outputList.append(inputList[i])
+            inputList[i] += 5
 else:
     for i in range(n):
-        if i % 2 == 0:
-            outputList.append(inputList[i])
-        else:
-            outputList.append(inputList[i] + 10)
+        if i % 2 == 1:
+            inputList[i] += 10
 
-print(outputList)
+print(inputList)
--- Zadanie 3/neMogaDaIzmislqIme.py
+++ Zadanie 3/neMogaDaIzmislqIme.py
@@ -1,12 +1,32 @@
+# # Old solution
+# n = int(input("Input number: "))
+# expression = ""
+
+# for i in range(n):
+#     for j in range(i + 1):
+#         expression += str(n)
+#     expression += " + "
+# expression = expression[:-3]
+
+# sum = eval(expression)
+
+# print("%s = %d" % (expression, sum))
+
+
+
+# New solution
 n = int(input("Input number: "))
 expression = ""
+addition = ""
+sum = 0
 
 for i in range(n):
     for j in range(i + 1):
         expression += str(n)
     expression += " + "
+    addition += str(n)
+    sum += int(addition)
+    print(sum)
 expression = expression[:-3]
-
-sum = eval(expression)
 
 print("%s = %d" % (expression, sum))
